328e3a2e66c60f29b5994e47752ca5242616f145
Александр <hyperpositron@gmail.com>
2023-02-11 18:02:23 +0200

    two commit hw13

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,34 @@
 public class Book {
+    private final String title;
+    private int publishingYear;
+    private final Author author;
+
+    public Book(String title, int publishingYear, Author author) {
+        this.title = title;
+        this.publishingYear = publishingYear;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+
+    public void printInfo() {
+        System.out.println("Название книги: " + getTitle() + " Год издания: " + getPublishingYear() + " , " +
+                "Автор: " + getAuthor().getName() + " " + getAuthor().getSurname());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Author lermontov = new Author("Михаил", "Лермонтов");
+        Author tolstoy = new Author("Лев", "Толстой");
 
+        Book mtsyri = new Book("Мцыри", 1839, lermontov);
+        Book warAndPeace = new Book("Война и мир", 1867, tolstoy);
+        mtsyri.printInfo();
+        warAndPeace.printInfo();
+
+        mtsyri.setPublishingYear(2023);
+        mtsyri.printInfo();
+    }
 }
